b13d3e029ec737a1f235dbdd97016e6db7510029
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-06-19 16:44:03 -0800

    Add fonts and logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import misionsReducer from './misions/misionsSlice';
 
-const store = configureStore({
-  reducer: {
-    misions: misionsReducer,
+const store = configureStore(
+  {
+    reducer: {
+      misions: misionsReducer,
+    },
   },
-});
+  applyMiddleware(logger),
+);
 
 export default store;
